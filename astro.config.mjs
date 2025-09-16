a235485614a05604c21b254c62f571f607e1e6c0
sullysaurus <dsully15@gmail.com>
2025-09-16 15:37:23 -0400

    fixing url share

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://backtothestart.band',
   integrations: [tailwind()],
   image: {
     service: {
